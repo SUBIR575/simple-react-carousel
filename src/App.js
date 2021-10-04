bcaa2c3050ee5b919ac912958dc03949cadcf734
Subir Pal <43574667+SUBIR575@users.noreply.github.com>
2021-10-04 22:24:45 +0530

    add css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
+import './App.css'
+import axios from 'axios';
+const App = () => {
+  const [Data,SetData] = useState([]);
+  const [Slide,SetSlide] = useState(0);
+  useEffect(() => {
+   axios.get('https://api.imgflip.com/get_memes')
+   .then((res)=>{
+      SetData(res.data.data.memes);
+      console.log(res);
+   })
+    
+  }, [SetData])
 
-function App() {
+ const length = Data.length;
+
+ const PrevSlide = ()=>{
+   SetSlide(Slide===0 ? length-1 : Slide-1);
+ }
+
+ const NextSlide = ()=>{
+   
+   SetSlide(Slide === length-1 ? 0 : Slide + 1);
+   debugger;
+   console.log(Slide);
+ }
+
+ console.log(Slide);
+
+ if (!Array.isArray(Data)|| Data.length<=0){
+   return null;
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main">
+    <button onClick={PrevSlide}>PREV</button>
+    {
+      Data.map((i,index)=>{
+        return(
+          <div key={index}>
+          {index===Slide &&(<img src={i.url} className="Img" alt="" />)}
+          </div>
+          )
+      }
+      )
+    }
+    <button onClick={NextSlide}>NEXT</button>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
